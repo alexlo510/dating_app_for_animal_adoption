5f696408e6c9c94d2eeb2b39c1c0ad9ed9631d26
Alex Lo <alexlo493@gmail.com>
2021-10-11 17:16:33 -0700

    added checkboxes for dispositions

--- frontend/src/components/adoptionCard.js
+++ frontend/src/components/adoptionCard.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material/';
+import { Button, Card, CardActions, CardContent, CardMedia, Checkbox, Typography } from '@mui/material/';
 
 export default function AdoptionCard(props) {
-    const{ name, description, type, breed, disposition, availability } = props.data
-    console.log(props)
+    const{ name, description, type, breed, dispositions, availability } = props.data
+    
     return (
         <Card sx={{}}>
             <CardMedia component="img" alt="" height="" image=""/>
             <CardContent>
                 <Typography gutterBottom variant="h5" component="div">{name}</Typography>
-                <Typography variant="body2" color="text.secondary">Type: {type}</Typography>
-                <Typography variant="body2" color="text.secondary">Breed: {breed}</Typography>
-                <Typography variant="body2" color="text.secondary">Disposition:</Typography>
-                <Typography gutterBottom variant="body2" color="text.secondary">Availability: {availability}</Typography>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant="body2" color="text.secondary" component="div">Type: {type}</Typography>
+                <Typography variant="body2" color="text.secondary" component="div">Breed: {breed}</Typography>
+                <Typography variant="body2" color="text.secondary" component="div">Disposition:
+                {Object.entries(dispositions).map(([key,value]) =>
+                    (value ? 
+                        (<Typography variant="body2" color="text.secondary" component="div">{key} <Checkbox size="small" disabled checked /></Typography>) : 
+                        (<Typography variant="body2" color="text.secondary" component="div">{key} <Checkbox size="small" disabled/></Typography>)
+                    )
+                )}
+                </Typography>
+                <Typography gutterBottom variant="body2" color="text.secondary" component="div">Availability: {availability}</Typography>
+                <Typography variant="body2" color="text.secondary" component="div">
                     {description}
                 </Typography>
             </CardContent>
--- frontend/src/pages/Adopt.js
+++ frontend/src/pages/Adopt.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Button, Grid, Container, Typography } from '@mui/material/';
+import { Button, Container, Grid} from '@mui/material/';
 import AdoptionCard from '../components/adoptionCard';
 
 const sampleData = {
@@ -7,7 +7,11 @@ const sampleData = {
     "description" : "chonky boy",
     "type": "Cat",
     "breed": "Tabby",
-    "disposition" : [],
+    "dispositions" :{
+        "Good with other animals" : false,
+        "Good with other children" : false,
+        "Animal must be leashed at all times" : true,
+    },
     "availability": "Available",
 }
 
@@ -18,16 +22,18 @@ export default function Adopt() {
     return (
         <>
             <Container>
-                <Grid container justifyContent="space-around" sx={{marginTop: 8}}>
+                <Grid container justifyContent="space-around" sx={{marginTop: 6}}>
                     <Grid item xs={8} md={6} lg={8}>
                         <AdoptionCard data={sampleData}/>
                     </Grid>
                 </Grid>
                 <Grid container justifyContent="center" sx={{marginTop: 1}}>
-                    <Grid item xs={2} md={1.5} lg={1.5}>
+                    <Grid item xs={2} md={2} lg={2}>
                         <Button>Left</Button>
                     </Grid>
-                    <Grid item xs={2} md={1.5} lg={1.5}>
+                    <Grid item xs={2} md={1} lg={1}>
+                    </Grid>
+                    <Grid item xs={2} md={1} lg={1}>
                         <Button>Right</Button>
                     </Grid>
                 </Grid>
@@ -35,13 +41,3 @@ export default function Adopt() {
         </>
     );
 }
-
-/*
-<Container>
-                <Grid container>
-                    <Grid item>
-                        <AdoptionCard data={sampleData}/>
-                    </Grid>
-                </Grid>
-            </Container>
-*/
