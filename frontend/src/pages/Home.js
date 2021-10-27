8d274d3b1a3e1f2d5260403fc0ab249f9b96deb7
Alex Lo <alexlo493@gmail.com>
2021-10-27 15:32:39 -0700

    added a image and welcome message to home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,39 @@
-import { Typography } from '@mui/material/';
+import { Box, Typography } from '@mui/material/';
+
+const backgroundStyle = {
+    backgroundImage: `url(${process.env.PUBLIC_URL}/assets/images/cat_dog.jpg)`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+}
+
+const welcomeMessageContainerStyle = {
+    textAlign: "center",
+    color: "Teal",
+}
+
+const wordStyle = {
+    fontSize: {lg: 100, md: 50, sm: 50, xs: 50},
+    fontFamily: "Marker Felt"
+}
 
 export default function Home() {
     return (
-        <div>
-            <Typography variant="h1"> Home Page</Typography>
-        </div>
+        <>
+            <div style={backgroundStyle}>
+                <Box sx={welcomeMessageContainerStyle}>
+                    <Typography variant="h1" sx={wordStyle}>Welcome to Pinder</Typography>
+                </Box>
+            </div>
+            {/* <div>
+                <Box sx={welcomeMessageContainerStyle}>
+                    <Typography variant="h1" sx={wordStyle}>Latest News</Typography>
+                </Box>
+            </div> */}
+        </>
     );
 }
