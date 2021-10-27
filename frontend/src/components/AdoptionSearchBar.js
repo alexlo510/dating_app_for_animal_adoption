74b59828695ebbcd1c9751ea4eae94bfb0f4c0dd
Alex Lo <alexlo493@gmail.com>
2021-10-26 20:46:09 -0700

    added checkbox for disposition

--- frontend/src/components/AdoptionSearchBar.js
+++ frontend/src/components/AdoptionSearchBar.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
-import { Button, FormControl, Grid, InputLabel, MenuItem, TextField, Select } from '@mui/material/';
-import { animalTypes, animalBreeds, disposition } from '../components/ProfilePropertiesLists.js';
+import { Button, Checkbox, FormControl, Grid, InputLabel, ListItemText, MenuItem, OutlinedInput, TextField, Select } from '@mui/material/';
+import { animalTypes, animalBreeds, dispositions } from '../components/ProfilePropertiesLists.js';
 
 const textFieldStyle = {
     marginRight: 2,
@@ -24,18 +24,47 @@ const dateStyle = {
     marginRight: 2,
 }
 
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 325,
+    },
+  },
+};
+
 export default function AdoptionSearchBar() {
 
     const [type, setType] = useState('')
     const [breed, setBreed] = useState('')
-    const [disposition, setDisposition] = useState('')
+    const [disposition, setDisposition] = useState([])
     const [date, setDate] = useState('')
 
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(type, breed, disposition, date);
+        // change disposition to an object?
+        let dispositionObject = {}
+        dispositions.forEach((key) => {
+            disposition.includes(key) ? dispositionObject[key] = true : dispositionObject[key] = false
+        });
+
+        console.log(dispositionObject);
+
+        console.log({type, breed, disposition, date});
         // call a function on the adopt page, passing in these parameters? 
+    }
+
+    const handleDisposition = (event) => {
+        const {
+            target: { value },
+          } = event;
+          setDisposition(
+            // On autofill we get a the stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+          );
     }
 
     return (
@@ -73,7 +102,27 @@ export default function AdoptionSearchBar() {
                                 ))}
                             </Select>
                         </FormControl>
-                        <FormControl sx={selectStyle}>
+                        <FormControl sx={{ m: 1, width: 325 }}>
+                            <InputLabel id="dispositionLabel">Disposition</InputLabel>
+                            <Select
+                            labelId="dispositionLabel"
+                            id="disposition"
+                            multiple
+                            value={disposition}
+                            onChange={handleDisposition}
+                            input={<OutlinedInput label="Disposition" />}
+                            renderValue={(selected) => selected.join(', ')}
+                            MenuProps={MenuProps}
+                            >
+                            {dispositions.map((item) => (
+                                <MenuItem key={item} value={item}>
+                                    <Checkbox checked={disposition.indexOf(item) > -1} />
+                                    <ListItemText primary={item} />
+                                </MenuItem>
+                            ))}
+                            </Select>
+                        </FormControl>
+                        {/* <FormControl sx={selectStyle}>
                             <InputLabel id="selectLabel">Disposition</InputLabel>
                             <Select
                                 labelId= "selectLabel"
@@ -87,7 +136,7 @@ export default function AdoptionSearchBar() {
                                 <MenuItem value={"Good with children"}>Good with Children</MenuItem>
                                 <MenuItem value={"Animals must be leashed at all times"}>Animals Must Be Leashed at All Times</MenuItem>
                             </Select>
-                        </FormControl>
+                        </FormControl> */}
                         <TextField
                             label="Date Created"
                             type="date"
