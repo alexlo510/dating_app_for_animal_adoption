cbf7077e45f1f0b218ac60b56f301b64feb90869
Alex Lo <alexlo493@gmail.com>
2021-11-11 21:55:34 -0800

    changed axios endpoint

--- frontend/src/components/UserContext.js
+++ frontend/src/components/UserContext.js
@@ -13,7 +13,13 @@ export function UserProvider({ children }) {
     useEffect(() => {
         try {
             async function fetchUser() {
-                const res = await Axios.get("https://pet-shelter-api.uw.r.appspot.com/getProfile", { withCredentials: true })
+                const windowUrl = window.location.search;
+                const params = new URLSearchParams(windowUrl);
+                console.log(params.get('owner_id')); // remove later
+                const owner_id = params.get('owner_id')
+                const res = await Axios.get(`https://pet-shelter-api.uw.r.appspot.com/getProfile?owner_id=${owner_id}`, { withCredentials: true} )
+                //const res = await Axios.get("https://pet-shelter-api.uw.r.appspot.com/getProfile", { withCredentials: true} )
+                console.log(res);
                 console.log(res.data); // remove after testing
                 if (res.data) {
                     setUser(res.data)
