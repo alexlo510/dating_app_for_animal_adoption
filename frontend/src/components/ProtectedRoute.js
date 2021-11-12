6244991e1a8a441345a579893f31207558ae1c24
Alex Lo <alexlo493@gmail.com>
2021-11-12 12:25:55 -0800

    refactored code

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-export default function ProtectedRoute( {isAuth: isAuth, component: Component, ...rest} ) {
+export default function ProtectedRoute( {isAuth, component: Component, ...rest} ) {
     return (
         <Route
             {...rest}
             render={(props) => {
                 return isAuth ? <Component {...props}/> 
-                : <Redirect to={{pathname: "/", state: {from: props.location} }} />
+                : <Redirect to="/" />
             }}
         />
     );
