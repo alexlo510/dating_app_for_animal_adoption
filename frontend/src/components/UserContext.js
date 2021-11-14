1f7baf14ecdd8e3cbbe9d6824b04e878443649fc
Alex Lo <alexlo493@gmail.com>
2021-11-13 21:12:51 -0800

    changed owner_id to accesstoken

--- frontend/src/components/UserContext.js
+++ frontend/src/components/UserContext.js
@@ -15,9 +15,9 @@ export function UserProvider({ children }) {
             async function fetchUser() {
                 const windowUrl = window.location.search;
                 const params = new URLSearchParams(windowUrl);
-                console.log(params.get('owner_id')); // remove later
-                const owner_id = params.get('owner_id')
-                const res = await Axios.get(`https://pet-shelter-api.uw.r.appspot.com/getProfile?owner_id=${owner_id}`, { withCredentials: true} )
+                console.log(params.get('accesstoken')); // remove later
+                const accesstoken = params.get('accesstoken')
+                const res = await Axios.get(`https://pet-shelter-api.uw.r.appspot.com/getProfile?accesstoken=${accesstoken}`, { withCredentials: true} )
                 //const res = await Axios.get("https://pet-shelter-api.uw.r.appspot.com/getProfile", { withCredentials: true} )
                 console.log(res.data); // remove after testing
                 if (res.data) {
